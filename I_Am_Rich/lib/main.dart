e6256c3310300ff89dc1fa55957cc897882beacc
Mihir-Achyuta <mihirachyuta@gmail.com>
2020-10-29 01:57:10 -0700

    Finished I_Am_Rich App

--- I_Am_Rich/lib/main.dart
+++ I_Am_Rich/lib/main.dart
@@ -1,13 +1,24 @@
 // import 'dart:html';
-
 import 'package:flutter/material.dart';
 
 // when flutter runs, it starts at main => MaterialApp
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text("Hi"),
+      // removes the debug banner on top right
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey[900],
+          centerTitle: true,
+          title: Text("I Am Rich"),
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage("images/diamond.png"),
+          ),
+        ),
       ),
     ),
   );
