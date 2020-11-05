ca2fa829d8c84ea14708872ce32176cd7b026a0a
Mihir-Achyuta <mihirachyuta@gmail.com>
2020-11-05 00:40:09 -0800

    Finished Magic_8_Ball App

--- Magic_8_Ball/lib/main.dart
+++ Magic_8_Ball/lib/main.dart
@@ -1,7 +1,54 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  runApp(BallPage());
+}
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue[900],
+          title: Text("Ask Me Anything"),
+        ),
+        body: Ball(),
       ),
     );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballFace = Random().nextInt(5) + 1;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: FlatButton(
+              onPressed: changeBallFace,
+              child: Image.asset("images/ball$ballFace.png"),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void changeBallFace() {
+    setState(() {
+      ballFace = Random().nextInt(5) + 1;
+    });
+  }
+}
