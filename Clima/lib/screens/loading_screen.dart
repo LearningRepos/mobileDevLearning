c5306cc16b72135c513d43e9e2bff57ba460a0f8
Mihir-Achyuta <mihirachyuta@gmail.com>
2020-11-18 14:17:44 -0800

    Added option for user to find location with gps

--- Clima/lib/screens/loading_screen.dart
+++ Clima/lib/screens/loading_screen.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
 
+import "package:clima/services/location.dart";
+
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  Location location = new Location();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: RaisedButton(
           onPressed: () {
-            //Get the current location
+            location.getCurrentLocation();
           },
           child: Text('Get Location'),
         ),
---
+++ Clima/lib/services/location.dart
@@ -0,0 +1,19 @@
+import 'package:geolocator/geolocator.dart';
+
+class Location {
+  double _latitude;
+  double _longitude;
+
+  void getCurrentLocation() async {
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high,
+      );
+      _latitude = position.latitude;
+      _longitude = position.longitude;
+      print("The latitude is $_latitude and the longitude is $_longitude");
+    } catch (e) {
+      print(e);
+    }
+  }
+}
