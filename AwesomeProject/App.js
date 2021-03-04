8a3f16ce6a2714d62eaf6a09617605a001d89011
Mihir-Achyuta <mihirachyuta@gmail.com>
2021-03-04 00:35:40 -0800

    Added unvalidated palette creation modal

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,23 +1,43 @@
-import "react-native-gesture-handler";
 import React from "react";
+import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
 import Home from "./screens/Home";
 import ColorPalette from "./screens/ColorPalette";
-import { createStackNavigator } from "@react-navigation/stack";
+import ColorPaletteModal from "./screens/ColorPaletteModal";
 
 class App extends React.Component {
   render() {
-    const Stack = createStackNavigator();
-    return (
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={Home} />
-          <Stack.Screen
+    const RootStack = createStackNavigator();
+    const MainStack = createStackNavigator();
+
+    const MainStackScreen = () => {
+      return (
+        <MainStack.Navigator>
+          <MainStack.Screen name="Home" component={Home} />
+          <MainStack.Screen
             name="ColorPalette"
             component={ColorPalette}
             options={({ route }) => ({ title: route.params.paletteName })}
           />
-        </Stack.Navigator>
+        </MainStack.Navigator>
+      );
+    };
+
+    return (
+      <NavigationContainer>
+        <RootStack.Navigator mode="modal">
+          <RootStack.Screen
+            name="Main"
+            component={MainStackScreen}
+            options={{ headerShown: false }}
+          />
+          <RootStack.Screen
+            name="Add New Palette"
+            component={ColorPaletteModal}
+          />
+        </RootStack.Navigator>
       </NavigationContainer>
     );
   }
--- AwesomeProject/screens/Home.js
+++ AwesomeProject/screens/Home.js
@@ -1,65 +1,70 @@
 import React from "react";
-import { View } from "react-native";
+import { View, FlatList, Text, TouchableOpacity } from "react-native";
 import ColorPreview from "../components/ColorPreview";
+import axios from "axios";
 
 class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { palettes: [], refreshing: false };
+    this.addPalette = this.addPalette.bind(this);
+  }
+  componentDidMount() {
+    axios
+      .get("https://color-palette-api.kadikraman.now.sh/palettes")
+      .then(({ data }) => this.setState({ palettes: data }));
+  }
+  addPalette(newColorArray, paletteName) {
+    let oldColorArray = newColorArray;
+    let rawColorArray = oldColorArray.filter(
+      (color, index) => color["selected"] === true
+    );
+    this.setState({
+      palettes: [
+        ...this.state.palettes,
+        { colors: rawColorArray, paletteName: paletteName },
+      ],
+    });
+  }
   render() {
     const { navigation } = this.props;
     return (
       <View>
-        <ColorPreview
-          colors={SOLARIZED}
-          colorHeader={"Solarized"}
-          navigation={navigation}
-        />
-        <ColorPreview
-          colors={RAINBOW}
-          colorHeader={"Rainbow"}
-          navigation={navigation}
-        />
-        <ColorPreview
-          colors={FRONTEND_MASTERS}
-          colorHeader={"Frontend Masters"}
-          navigation={navigation}
+        <FlatList
+          data={this.state.palettes}
+          keyExtractor={(item) => item.paletteName}
+          renderItem={({ item }) => {
+            return (
+              <ColorPreview
+                colors={item.colors}
+                colorHeader={item.paletteName}
+                navigation={navigation}
+              />
+            );
+          }}
+          ListHeaderComponent={
+            <TouchableOpacity
+              onPress={() =>
+                navigation.navigate("Add New Palette", this.addPalette)
+              }
+            >
+              <Text
+                style={{
+                  fontWeight: "bold",
+                  fontSize: 20,
+                  marginHorizontal: 10,
+                  color: "teal",
+                  marginTop: 10,
+                }}
+              >
+                Add a color scheme
+              </Text>
+            </TouchableOpacity>
+          }
         />
       </View>
     );
   }
 }
-
-const SOLARIZED = [
-  { colorName: "Base03", hexCode: "#002b36" },
-  { colorName: "Base02", hexCode: "#073642" },
-  { colorName: "Base01", hexCode: "#586e75" },
-  { colorName: "Base00", hexCode: "#657b83" },
-  { colorName: "Base0", hexCode: "#839496" },
-  { colorName: "Base1", hexCode: "#93a1a1" },
-  { colorName: "Base2", hexCode: "#eee8d5" },
-  { colorName: "Base3", hexCode: "#fdf6e3" },
-  { colorName: "Yellow", hexCode: "#b58900" },
-  { colorName: "Orange", hexCode: "#cb4b16" },
-  { colorName: "Red", hexCode: "#dc322f" },
-  { colorName: "Magenta", hexCode: "#d33682" },
-  { colorName: "Violet", hexCode: "#6c71c4" },
-  { colorName: "Blue", hexCode: "#268bd2" },
-  { colorName: "Cyan", hexCode: "#2aa198" },
-  { colorName: "Green", hexCode: "#859900" },
-];
-
-const RAINBOW = [
-  { colorName: "Red", hexCode: "#FF0000" },
-  { colorName: "Orange", hexCode: "#FF7F00" },
-  { colorName: "Yellow", hexCode: "#FFFF00" },
-  { colorName: "Green", hexCode: "#00FF00" },
-  { colorName: "Violet", hexCode: "#8B00FF" },
-];
-
-const FRONTEND_MASTERS = [
-  { colorName: "Red", hexCode: "#c02d28" },
-  { colorName: "Black", hexCode: "#3e3e3e" },
-  { colorName: "Grey", hexCode: "#8a8a8a" },
-  { colorName: "White", hexCode: "#ffffff" },
-  { colorName: "Orange", hexCode: "#e66225" },
-];
 
 export default Home;
